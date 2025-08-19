c9401bf0b2118807481fc1563fd7a2e681170236
Priyanka7685 <priyankathakur7685@gmail.com>
2025-08-19 21:03:13 +0530

    updated code

--- backend/src/controllers/profileController.js
+++ backend/src/controllers/profileController.js
@@ -152,7 +152,9 @@ const createProfile = async(req, res) => {
     const loggedInUser = await User.findById(loggedInUserId).select("following");
 
     // map profiles into a neat JSON response
-    const usersWithFollowStatus = profiles.map((p) => ({
+    const usersWithFollowStatus = profiles
+    .filter(p => p.user)
+    .map((p) => ({
       _id: p.user._id,
       username: p.user.username,
       email: p.user.email,
